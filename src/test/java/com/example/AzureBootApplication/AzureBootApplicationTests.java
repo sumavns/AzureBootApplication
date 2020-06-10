97909d17faf479a235098851eb746c6e7c6160c0
Sumalatha_V <Sumalatha_V@BLRKEC120294L.ad.infosys.com>
2020-06-10 14:33:30 -0400

    committing files

---
+++ src/test/java/com/example/AzureBootApplication/AzureBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureBootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
